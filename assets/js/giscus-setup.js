32b967660c96b9651d2470322a00358a22106c8b
zhai-yujia <68308586+zhai-yujia@users.noreply.github.com>
2026-03-07 16:32:02 +0000

    Deploying to gh-pages from @ zhai-yujia/zhai-yujia.github.io@8c907b36d1cfbca466d5ce30d00e0d18be54ebb0 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "zhai-yujia/yzhai.github.io",
+    "data-repo": "zhai-yujia/zhai-yujia.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
